43600460e2add089c4f25fa83f8d0fa4da41077b
fuzorh <uzorhfrancis@gmail.com>
2022-10-03 12:27:45 -0400

    Add info table

--- src/components/InfoTable.js
+++ src/components/InfoTable.js
@@ -6,60 +6,66 @@ import { resources } from "../data/resources.js";
 
 function InfoTable(props) {
     return (
-    <Card style={{ maxWidth: "92.5rem", margin: "4rem", overflow: "auto" }}>
-        <Table striped bordered hover size="sm">
-            <thead>
-                <tr style={{ borderBottom: "solid 0.1rem" }}>
-                    <th style={{ borderRight: "solid 0.05rem" }}>Word</th>
-                    <th>Description</th>
-                </tr>
-            </thead>
-            <tbody>
-                {wordDescriptions.map((wordDescriptions) => {
-                    return (
-                        <tr>
-                            <td style={{ borderRight: "solid 0.05em" }}>{wordDescriptions.Word}</td>
-                            <td>{wordDescriptions.Description}</td>
+        <div>
+            <Card style={{ maxWidth: "92.5rem", margin: "4rem", overflow: "auto" }}>
+                <Table striped bordered hover size="sm">
+                    <thead>
+                        <tr style={{ borderBottom: "solid 0.1rem" }}>
+                            <th style={{ borderRight: "solid 0.05rem" }}>Word</th>
+                            <th>Description</th>
                         </tr>
-                    );
-                })}
-            </tbody>
-        </Table>
-        <Table striped bordered hover size="sm">
-            <thead>
-                <tr style={{ borderBottom: "solid 0.1rem" }}>
-                    <th style={{ borderRight: "solid 0.05rem" }}>Abrevation</th>
-                    <th>Definition</th>
-                </tr>
-            </thead>
-            <tbody>
-                {abbreviations.map((abbreviations) => {
-                    return (
-                        <tr>
-                            <td style={{ borderRight: "solid 0.05em" }}>{abbreviations.Abrevation}</td>
-                            <td>{abbreviations.Definition}</td>
+                    </thead>
+                    <tbody>
+                        {wordDescriptions.map((wordDescriptions) => {
+                            return (
+                                <tr>
+                                    <td style={{ borderRight: "solid 0.05em" }}>{wordDescriptions.Word}</td>
+                                    <td>{wordDescriptions.Description}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </Table>
+            </Card>
+            <Card style={{ maxWidth: "92.5rem", margin: "4rem", overflow: "auto" }}>
+                <Table striped bordered hover size="sm">
+                    <thead>
+                        <tr style={{ borderBottom: "solid 0.1rem" }}>
+                            <th style={{ borderRight: "solid 0.05rem" }}>Abrevation</th>
+                            <th>Definition</th>
                         </tr>
-                    );
-                })}
-            </tbody>
-        </Table>
-        <Table striped bordered hover size="sm">
-            <thead>
-                <tr style={{ borderBottom: "solid 0.1rem" }}>
-                    <th style={{ borderRight: "solid 0.05rem" }}>Resources</th>
-                </tr>
-            </thead>
-            <tbody>
-                {resources.map((resources) => {
-                    return (
-                        <tr>
-                            <td style={{ borderRight: "solid 0.05em" }}>{resources.Reference}</td>
+                    </thead>
+                    <tbody>
+                        {abbreviations.map((abbreviations) => {
+                            return (
+                                <tr>
+                                    <td style={{ borderRight: "solid 0.05em" }}>{abbreviations.Abrevation}</td>
+                                    <td>{abbreviations.Definition}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </Table>
+            </Card>
+            <Card style={{ maxWidth: "92.5rem", margin: "4rem", overflow: "auto" }}>
+                <Table striped bordered hover size="sm">
+                    <thead>
+                        <tr style={{ borderBottom: "solid 0.1rem" }}>
+                            <th style={{ borderRight: "solid 0.05rem" }}>Resources</th>
                         </tr>
-                    );
-                })}
-            </tbody>
-        </Table>
-    </Card>
+                    </thead>
+                    <tbody>
+                        {resources.map((resources) => {
+                            return (
+                                <tr>
+                                    <td style={{ borderRight: "solid 0.05em" }}>{resources.Reference}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </Table>
+            </Card>
+        </div>
     );
 }
 
