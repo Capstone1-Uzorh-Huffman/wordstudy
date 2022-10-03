856cb8083889fc06acb6c375cc7195834cfa7759
Joshua Huffman <jwhuffman@anderson.edu>
2022-10-03 12:12:20 -0400

    Adding files

---
+++ src/data/resources.js
@@ -0,0 +1,33 @@
+export const resources = [
+    
+    {
+        Reference: "Balota, D. A., Yap, M. J., Cortese, M. J., Hutchison, K. A., Kessler, B., Loftis, B., … Treiman, R. (2007). The English Lexicon Project. Behavior Research Methods, 39(3), 445–459. https://doi.org/10.3758/BF03193014"
+    },
+    {
+        Reference: "Brysbaert, M., & New, B. (2009). Moving beyond Kucera and Francis: A critical evaluation of current word frequency norms and the introduction of a new and improved word frequency measure for American English. Behavior Research Methods, 41(4), 977–90. https://doi.org/10.3758/BRM.41.4.977"
+    },
+    {
+        Reference: "Clark, J. M., & Paivio, A. (2004). Extensions of the Paivio, Yuille, and Madigan (1968) norms. Behavior Research Methods, Instruments, & Computers, 36(3), 371–383. https://doi.org/10.3758/BF03195584"
+    },
+    {
+        Reference: "Coltheart, M. (1981). The MRC psycholinguistic database. The Quarterly Journal of Experimental Psychology, 33(4), 497–505. https://doi.org/10.1080/14640748108400805"
+    },
+    {
+        Reference: "Friendly, M., Franklin, P. E., Hoffman, D., & Rubin, D. C. (1982). The Toronto Word Pool: Norms for imagery, concreteness, orthographic variables, and grammatical usage for 1,080 words. Behavior Research Methods & Instrumentation, 14(4), 375–399. https://doi.org/10.3758/BF03203275"
+    },
+    {
+        Reference: "Kuperman, V., Stadthagen-Gonzalez, H., & Brysbaert, M. (2012). Age-of-acquisition ratings for 30,000 English words. Behavior Research Methods, 44, 978–990. https://doi.org/10.3758/s13428-012-0210-4"
+    },
+    {
+        Reference: "Nelson, D. L., McEvoy, C. L., & Schreiber, T. A. (1998). The University of south Florida word association, rhyme, and word fragment norms. Retrieved from http://www.usf.edu/FreeAssociation/ https://doi.org/10.3758/BF03195588"
+    },
+    {
+        Reference: "Schock, J., Cortese, M. J., & Khanna, M. M. (2012). Imageability estimates for 3,000 disyllabic words. Behavior Research Methods, 44(2), 374–9. https://doi.org/10.3758/s13428-011-0162-0"
+    },
+    {
+        Reference: "Stadthagen-Gonzalez, H., & Davis, C. J. (2006). The Bristol norms for age of acquisition, imageability, and familiarity. Behavior Research Methods, 38(4), 598–605. https://doi.org/10.3758/BF03193891"
+    },
+    {
+        Reference: "Warriner, A. B., Kuperman, V., & Brysbaert, M. (2013). Norms of valence, arousal, and dominance for 13,915 English lemmas. Behavior Research Methods, 45 VN - r(4), 1191–1207. https://doi.org/10.3758/s13428-012-0314-x"
+    }
+]
